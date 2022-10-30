0fcde58b4688ba86fabc15198f69549f2501e578
joyfuldev <wings2132@naver.com>
2022-10-30 21:11:35 +0900

    도시별 api 가져와서 UI에 출력

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import WeatherBox from "./components/WeatherBox";
 import WeatherButton from "./components/WeatherButton";
+import ClipLoader from "react-spinners/ClipLoader";
 
 function App() {
   const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState("");
+  const [loading, setLoading] = useState(false);
+  const cities = ["seoul", "busan", "new york", "paris"];
 
   useEffect(() => {
-    getCurrentLocation();
-  }, []);
+    if (city === "") {
+      getCurrentLocation();
+    } else {
+      getWeatherByCity();
+    }
+  }, [city]);
+
+  const getWeatherByCity = async () => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=metric`;
+    setLoading(true);
+    const response = await fetch(url);
+    const data = await response.json();
+    setWeather(data);
+    setLoading(false);
+  };
 
   const getCurrentLocation = () => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -21,17 +39,31 @@ function App() {
 
   const getWeatherByCurrentLocation = async (lat, lon) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}&units=metric`;
+    setLoading(true);
     let response = await fetch(url);
     let data = await response.json();
     setWeather(data);
+    setLoading(false);
   };
 
   return (
     <div>
-      <div className="container">
-        <WeatherBox weather={weather} />
-        <WeatherButton current={getCurrentLocation} setWeather={setWeather} />
-      </div>
+      {loading ? (
+        <div className="container">
+          <ClipLoader
+            color="#f9ca24"
+            loading={loading}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        <div className="container">
+          <WeatherBox weather={weather} />
+          <WeatherButton cities={cities} setCity={setCity} />
+        </div>
+      )}
     </div>
   );
 }
--- src/components/WeatherButton.js
+++ src/components/WeatherButton.js
@@ -1,29 +1,14 @@
-import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
-const WeatherButton = ({ current, setWeather }) => {
-  const getWeatherByCity = async (city) => {
-    if (city !== "current") {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=metric`;
-      const response = await fetch(url);
-      const data = await response.json();
-      setWeather(data);
-    }
-  };
+const WeatherButton = ({ cities, setCity }) => {
   return (
     <div>
-      <Button variant="warning" onClick={current}>
-        내 위치
-      </Button>
-      <Button variant="warning" onClick={() => getWeatherByCity("new york")}>
-        뉴욕
-      </Button>
-      <Button variant="warning" onClick={() => getWeatherByCity("paris")}>
-        파리
-      </Button>
-      <Button variant="warning" onClick={() => getWeatherByCity("bangkok")}>
-        방콕
-      </Button>
+      <Button variant="warning">내 위치</Button>
+      {cities.map((city, index) => (
+        <Button variant="warning" key={index} onClick={() => setCity(city)}>
+          {city}
+        </Button>
+      ))}
     </div>
   );
 };
